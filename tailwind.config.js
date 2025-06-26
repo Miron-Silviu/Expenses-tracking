72a5d7fcab8942bbf0315dcfdb9bd5becdd5ed44
Silviu <silviu_miron_90@outlook.com>
2025-06-26 10:31:23 +0100

    Install tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,9 @@
-
 // remember to use module.exports instead of tailwind.config in production
-tailwind.config = 
-   {
-      // Note: config only includes the used styles & variables of your selection
-      content: ["./src/**/*.{html,vue,svelte,js,ts,jsx,tsx}"],
-      theme: {
-        extend: {
-          fontFamily: {
-            
-          },
-          fontSize: {
-            
-          },
-          fontWeight: {
-            
-          },
-          lineHeight: {
-             
-          },
-          letterSpacing: {
-             
-          },
-          borderRadius: {
-              
-          },
-          colors: {
-            
-            
-          },
-          spacing: {
-              
-          },
-          width: {
-             
-          },
-          minWidth: {
-             
-          },
-          maxWidth: {
-             
-          },
-          height: {
-             
-          },
-          minHeight: {
-             
-          },
-          maxHeight: {
-             
-          }
-        }
-      }
-    }
-
-          
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./docs/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
