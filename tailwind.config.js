5a9cdff35f0d77ca955eee0936ce95750c441b2e
Silviu <silviu_miron_90@outlook.com>
2025-06-26 20:32:57 +0100

    Feat: Implement screens responsivness.
    -Update tailwind.config.js with different screen sizes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 // remember to use module.exports instead of tailwind.config in production
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./docs/**/*.{html,js}'],
   theme: {
+    screens: {
+      xs: '300px',
+      sm: '301px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '2000px',
+      '4xl': '2600px', // ← missing px here
+    },
     fontSize: {
       sm: ['7px', '10px'],
       base: ['16px', '24px'],
