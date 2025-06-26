653ee86d5fb9d86e013cd852a4af8ce9531451bc
Silviu <silviu_miron_90@outlook.com>
2025-06-26 10:01:36 +0100

    Initial commit Set up the project .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,56 @@
+
 // remember to use module.exports instead of tailwind.config in production
-// tailwind.config.js
-module.exports = {
-  content: ['./docs/**/*.{html,vue,svelte,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {},
-      fontSize: {},
-      fontWeight: {},
-      lineHeight: {},
-      letterSpacing: {},
-      borderRadius: {
-        // Example valid value:
-        'var-sds-size-radius-full': '9999px',
-      },
-      colors: {},
-      spacing: {},
-      width: {},
-      minWidth: {},
-      maxWidth: {},
-      height: {},
-      minHeight: {},
-      maxHeight: {},
-    },
-  },
-};
+tailwind.config = 
+   {
+      // Note: config only includes the used styles & variables of your selection
+      content: ["./src/**/*.{html,vue,svelte,js,ts,jsx,tsx}"],
+      theme: {
+        extend: {
+          fontFamily: {
+            
+          },
+          fontSize: {
+            
+          },
+          fontWeight: {
+            
+          },
+          lineHeight: {
+             
+          },
+          letterSpacing: {
+             
+          },
+          borderRadius: {
+              
+          },
+          colors: {
+            
+            
+          },
+          spacing: {
+              
+          },
+          width: {
+             
+          },
+          minWidth: {
+             
+          },
+          maxWidth: {
+             
+          },
+          height: {
+             
+          },
+          minHeight: {
+             
+          },
+          maxHeight: {
+             
+          }
+        }
+      }
+    }
+
+          
