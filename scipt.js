3b1d314109b500ec0d64255650837e564a423bf9
Silviu <silviu_miron_90@outlook.com>
2025-07-01 13:15:43 +0100

    feat: implement "Add Category Row" button functionality (P0, size M, iteration 1 ).
    Implement the button that allow users to add a new categories row.

--- scipt.js
+++ scipt.js
@@ -7,7 +7,4 @@ const clearButton = document.querySelector('.clear_all');
 const rowCategoriesBtn = document.querySelector('.row_categories');
 const categoriesTable = document.querySelector('.categories_table_body');
 
-rowCategoriesBtn.addEventListener('click', function () {
-  const inputNumberValue = inputNumber.value;
-  console.log(inputNumberValue);
-});
+rowCategoriesBtn.addEventListener('click', function () {});
