a2aa472c5198b78076cedba79b5e956c8f450131
Silviu <silviu_miron_90@outlook.com>
2025-07-01 11:36:38 +0100

    Fix: Fix index.html

--- scipt.js
+++ scipt.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Variable Declarations
+
+const inputNumber = document.querySelector('.input_number');
+const clearButton = document.querySelector('.clear_all');
+const rowCategoriesBtn = document.querySelector('.row_categories');
+const categoriesTable = document.querySelector('.categories_table_body');
+
+rowCategoriesBtn.addEventListener('click', function () {
+  const inputNumberValue = inputNumber.value;
+  console.log(inputNumberValue);
+});
