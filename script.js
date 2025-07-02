672ad4d64e8f69eaa66c7c7d8a37505ea0ebf66b
Silviu <silviu_miron_90@outlook.com>
2025-07-02 17:36:05 +0100

    Feat: Variable declarations

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Variable Declarations
+
+const inputNumber = document.querySelector('.input_number');
+const clearButton = document.querySelector('.clear_all');
+const rowCategoriesBtn = document.querySelector('.row_categories');
+const categoriesTable = document.querySelector('.categories_table_body');
+let content = document.querySelectorAll('.tabel_data');
+let tableBody = document.querySelector('.categories_table_body');
